41735bd5998fb35dcbf116d44a40b8953b626212
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-27 08:04:00 +0530

    customer.service

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/Rx';
-import { Observable } from '@rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class CustomerService {
 
-  constructor() { }
+  constructor(private _http:Http) {
 
+   }
+   public getCountries():any{
+     return this._http.get("https://www.w3schools.com/angular/customers.php")
+    .map((res:Response)=>{
+      return res.json();
+    }).catch(this._handleError);
+   }
+   public _handleError(error){
+     console.log("Error raised" +error);
+     return Observable.throw(error || "Internal server error");
+   }
 }
