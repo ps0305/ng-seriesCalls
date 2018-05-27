005de616831a310b84a03e443e2eeb4f63c090bf
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-27 09:02:12 +0530

    final commit

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -9,7 +9,7 @@ export class CustomerService {
   constructor(private _http:Http) {
 
    }
-   public getCostomer():any{
+   public getCustomer():any{
      return this._http.get("https://www.w3schools.com/angular/customers.php")
      //catching positive response
     .map((res:Response)=>{
