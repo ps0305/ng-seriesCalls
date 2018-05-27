7b4743936c82244ea28ab0938a088d7a2d5e85f2
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-27 07:56:14 +0530

    service

---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { Observable } from '@rxjs/Observable';
+
+@Injectable()
+export class CustomerService {
+
+  constructor() { }
+
+}
